88077a830ed745bddd453fdcb725e436299dbcb0
Nicolás <nicogazzolo2398@hotmail.com>
2022-10-26 12:01:16 -0500

    Ya funciona el dashboard con el modelo de Administradores
    Se logra mostrar la tabla de admins de la
    DB, al igual que se inicia el intento de loggear a el usuario

--- proyecto/controllers/AdminController.js
+++ proyecto/controllers/AdminController.js
@@ -1,17 +1,29 @@
 // controllers/AdminController.js
-
-let Admin = require('../models/Admin')
+const Admin = require('../models/Admin')
 
 
 exports.home = function(req,res){
     res.render('homepage')
 }
 
-exports.login = function(req,res){
-    res.render('create')
+exports.login = (req, res) => {
+    
+    //Obtiene el body de homepage.pug
+    let admin = req.body.admin;
+
+    Admin.find(admin).then((admins) => {
+        //Si el usuario no coincide manda
+
+        if(admins.user != 'Nicolas' && admins.password != 'nico1234') {
+            res.status(404).send('Usuario y/o contraseña incorrectos');
+        }
+        //Si coincide se crea
+        res.render('create');
+    });
 }
 
-
-exports.dashboard = function(req,res){
-    res.render('dashboard')
+exports.dashboard = function(req, res){
+    Admin.find({}, function(err, usr){
+        res.render("dashboard", {users:usr})
+    })
 }
--- proyecto/models/Admin.js
+++ proyecto/models/Admin.js
@@ -7,4 +7,24 @@ let AdminSchema = new Schema({
     password: {type: String, required: true}
 })
 
+AdminSchema.statics.allAdmins = function allAdmins(cb){
+    /*
+    Usuarios.find({}, function(err, usr){
+        res.render("/",{user: usr})
+    })
+    */
+    return this.find({}, cb)
+} 
+
+AdminSchema.statics.createInstance = function(type, user, password){
+    return new this({
+        type: type,
+        user: user,
+        password: password
+    })
+}
+
+AdminSchema.statics.add = function(aUsr, cb){
+    this.create(aUsr, cb)
+}
 module.exports = mongoose.model('Admin', AdminSchema)
